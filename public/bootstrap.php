6e372f705b48c6d2b1fc7335df0e821de1e66d18
Pedro Henrique Pereira <pedro23henrique@hotmail.com>
2023-11-24 20:56:08 -0300

    FEAT: Implementa inativa erros php prod

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 use app\core\Router;
+use app\enums\Environment;
 
 require '../vendor/autoload.php';
 
@@ -10,3 +11,9 @@ session_start();
 // $dotenv->load();
 
 $route = new Router;
+
+if (Environment::Production->getEnvironment()) {
+    ini_set('display_erros', 0);
+    ini_set('display_startup_erros', 0);
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+}
