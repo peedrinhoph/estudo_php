0939e597c18cdb4155f104dc91689817a9b18391
Pedro Henrique Pereira <pedro23henrique@hotmail.com>
2023-11-24 09:16:19 -0300

    FEAT: Estudo

--- app/library/View.php
+++ app/library/View.php
@@ -4,6 +4,7 @@ namespace app\library;
 
 use Exception;
 use League\Plates\Engine;
+use app\library\CartInfo;
 
 class View
 {
@@ -12,26 +13,24 @@ class View
     private static function addInstances($instanceKey, $instanceClass)
     {
         if (!isset(self::$instances[$instanceKey])) {
-            self::$instances[$instanceKey] = new $instanceClass;
+            self::$instances[$instanceKey] = $instanceClass;
         }
     }
 
     public static function render(string $view, array $data = [])
     {
         $path = dirname(__FILE__, 3);
-
         $filePath = $path . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
-        ///var/www/app/views/nameView.php
+
         if (!file_exists($filePath . $view . '.php')) {
-            throw new Exception("View {$view} não existe");
+            throw new Exception("View {$view} does not exist");
         }
 
-        self::addInstances('cart', new CartInfo);
+        self::addInstances('cart', CartInfo::class);
+        self::addInstances('auth', Auth::class);
 
         $templates = new Engine($filePath);
-
         $templates->addData(['instances' => self::$instances]);
-
         echo $templates->render($view, $data);
     }
 }
